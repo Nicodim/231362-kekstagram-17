e88904263c88b6e5bcf70ee49c8422510213c69f
Nick <nicky.kravtsov@gmail.com>
2019-06-17 13:40:08 +0300

    правки параметров функций

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
-
+var name = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var url = function (n) {
-var photo = 'photos/{{i}}.jpg';
-for (var i = 0; i < n.length; i++) {
-  return photo[i];
-}
+return 'photos/' + n + '.jpg';
 }
 
 var likes = function (min, max) {
@@ -19,14 +16,31 @@ var likes = function (min, max) {
 }
 
 var getRandomComment = function (comment) {
-  var randomComment = comment[Math.floor(Math.random() * comment.length)];
+  var randomComment = comment[Math.floor(Math.random() * commentst.length)];
   return randomComment;
+};
+
+var getRandomName = function (Name) {
+  var randomName = Name[Math.floor(Math.random() * name.length)];
+  return randomName;
+};
+
+var getRandomAvatar = function (n) {
+return 'img/' + n + '.svg';
+}
+
+var commentArr = function (n) {
+  var user = [];
+  for (i = 0; i < n; i++) {
+    user[i] = {avatar: getRandomAvatar(n), message: getRandomComment(comment), name: getRandomName(Name)};
+  }
+  return user;
 };
 
 var createNewArr = function (n) {
   var ojectArr = [];
   for (i = 0; i < n; i++) {
-    ojectArr[i] = {url: function(photo), likes: function(random), comments: function(randomComment)};
+    ojectArr[i] = {url: url(n), likes: likes(min, max), comments: getRandomComment(comment)};
   }
   return ojectArr;
 };
